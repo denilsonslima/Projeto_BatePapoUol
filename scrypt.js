8dc5f0054bd6d90567216081df9796cf14424262
denilsonslima <denilsondslima@gmail.com>
2022-10-27 00:33:19 -0300

    sms do servidor adicionadas

--- scrypt.js
+++ scrypt.js
@@ -1,3 +1,5 @@
+// const name = prompt("Informe Seu Nome: ")
+
 function adicionarContato(seletor){
     const aparecer = document.querySelector(".ctt")
     aparecer.classList.remove("escondido")
@@ -8,4 +10,25 @@ function removerCtt(seletor){
     desaparecer.classList.add("escondido")
 }
 
-const sms = axios.
+let nomes = [];
+
+const promessa = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
+promessa.then(funcao)
+
+function funcao(valor){
+    nomes = valor.data;
+    rederizarReceitas()
+}
+
+function rederizarReceitas(){
+    let main = document.querySelector(".main")
+    main.innerHTML = "";
+    for (let i = 0; i < nomes.length; i++){
+        let name = nomes[i].from
+        let time = nomes[i].time
+        let text = nomes[i].text
+        main.innerHTML+= `
+        <div class="chat"><p><time>${time}</time> ${name} ${text}</p></div>
+        `
+    }
+}
